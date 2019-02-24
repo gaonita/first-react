6d1329416fe098434cc72cc3c1819006df26eca0
gaonita <gaon.yg@gmail.com>
2019-02-24 19:49:01 +0100

    first react project

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Header from './Header'
+import Footer from './Footer'
+import Hello from './Hello'
+import About from './About'
+import Projects from './Projects'
+import { Switch, Route} from 'react-router-dom'
+
+
+import './css/tailwind.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="page-wrapper">
+          <div className="page-content">
+              <Header />
+              <div className="content-wrapper">
+                  <Switch>
+                      <Route exact path ='/' component={Hello} ></Route>
+                      <Route exact path ='/about' component={About}></Route>
+                      <Route exact path ='/projects' component={Projects}></Route>
+                  </Switch>
+              </div>
+              <Footer/>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
