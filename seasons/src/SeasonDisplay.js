6fca3e692d641d99fef4ed90dec97d60fbd28e18
gaonita <gaon.yg@gmail.com>
2019-10-12 22:58:23 +0200

    react day 4

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SeasonDisplay = ( props ) => {
+    console.log(props.lat);
+    return <div>Display</div>
+};
+
+export default SeasonDisplay;
---
+++ seasons/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
+
+
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {
+    //         lat: null,
+    //         errorMessage: ''
+    //     };
+    // }
+
+    state = {lat:null, errorMessage:''};
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    //you have to define render!!
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat = {this.state.lat}/>
+        }
+
+        return <div> Loading </div>
+    };
+}
+
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
