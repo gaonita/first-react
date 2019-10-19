57dc5da19c5f76d4357164947d51137183e53e81
gaonita <gaon.yg@gmail.com>
2019-10-19 10:34:26 +0200

    react day 9

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyArpc9KjBex0bgw0PYJ1amTIcN0D8hD9c0';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part:'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
